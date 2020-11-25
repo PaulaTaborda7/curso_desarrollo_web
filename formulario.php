2e64a788c7baea83f6bf7a6888b1b492b2ff94db
PaulaTaborda7 <ptabordam@unal.edu.co>
2020-11-25 10:57:49 -0500

    Se suben los archivos

---
+++ formulario.php
@@ -0,0 +1,8 @@
+<?php
+
+$correo_formulario=$_POST['correo'];
+$contrasenia_formulario=$_POST['contrasenia'];
+
+echo "El correo que usted ingresó es ",$correo_formulario;
+echo "La contraseña que usted ingresó es ",$cntrasenia_formulario;
+?>
